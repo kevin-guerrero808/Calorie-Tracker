648697a49c2cf2feeba1a081250a776715cee538
kevin.guerrero <kevin.guerrero@newshore.es>
2022-06-06 21:18:14 -0500

    add implementation to calorietracker

--- src/calorieTracker.js
+++ src/calorieTracker.js
@@ -1,7 +1,20 @@
 const calculateWeightLostInAMonth = (cycling,running,swimming,extraCalorieInTake) =>{
+
+   if (cycling <= 0 || running <= 0 || swimming <= 0 || extraCalorieInTake <= 0) {
+      return -1;
+   }
+
    let weightLostInAMonth = 0;
 
    // write logic here 
+   const burnedCaloriesByCycling = cycling * 8; 
+   const burnedCaloriesBySwiming = swimming * 8;
+   const burnedCaloriesByRunning = running * 8;
+   const extraCalorieInTakeInMonth = extraCalorieInTake * 30;
+
+   const LostCarloriesInMonth = (burnedCaloriesByCycling + burnedCaloriesBySwiming + burnedCaloriesByRunning) - extraCalorieInTakeInMonth;
+
+   weightLostInAMonth = LostCarloriesInMonth / 1000;
 
    return weightLostInAMonth;
    
